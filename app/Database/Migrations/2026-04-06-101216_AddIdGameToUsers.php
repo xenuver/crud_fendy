19190a613a968beb4e735206b7a7e6fdd9a774a7
xenuver <auikwok2@gmail.com>
2026-07-03 20:31:26 +0700

    Fix: Add fieldExists check to AddIdGameToUsers migration to prevent Duplicate column error

--- app/Database/Migrations/2026-04-06-101216_AddIdGameToUsers.php
+++ app/Database/Migrations/2026-04-06-101216_AddIdGameToUsers.php
@@ -8,14 +8,16 @@ class AddIdGameToUsers extends Migration
 {
     public function up()
     {
-        $this->forge->addColumn('users', [
-            'id_game' => [
-                'type' => 'VARCHAR',
-                'constraint' => '50',
-                'after' => 'no_telp',
-                'null' => true,
-            ],
-        ]);
+        if (!$this->db->fieldExists('id_game', 'users')) {
+            $this->forge->addColumn('users', [
+                'id_game' => [
+                    'type' => 'VARCHAR',
+                    'constraint' => '50',
+                    'after' => 'no_telp',
+                    'null' => true,
+                ],
+            ]);
+        }
     }
 
     public function down()
