52cb0ee499b81025189d902cf193f23873e8c42e
xenuver <auikwok2@gmail.com>
2026-07-03 20:49:12 +0700

    Fix: Create settings table migration properly and sync setting_id in seeder

--- app/Database/Migrations/2026-04-13-124348_SettingsTable.php
+++ app/Database/Migrations/2026-04-13-124348_SettingsTable.php
@@ -8,11 +8,38 @@ class SettingsTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'setting_id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'sys_key' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'sys_value' => [
+                'type'       => 'TEXT',
+                'null'       => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        
+        $this->forge->addKey('setting_id', true);
+        $this->forge->addUniqueKey('sys_key');
+        $this->forge->createTable('settings');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('settings');
     }
 }
