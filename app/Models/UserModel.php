4d7059b25bc57c1a2b424c20e62bfb929360430a
fendy <fendy.fendy770@gmail.com>
2026-08-01 13:52:19 +0700

    fix: mendaftarkan kolom email ke allowedFields di UserModel agar data email tersimpan ke database

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -12,7 +12,7 @@ class UserModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ['username', 'no_telp', 'id_game', 'password', 'role'];
+    protected $allowedFields    = ['username', 'email', 'no_telp', 'id_game', 'password', 'role'];
 
     // Dates
     protected $useTimestamps = true;
