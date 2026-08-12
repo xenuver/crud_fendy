0e5e7df93eabd0d487a132e7ffc0a7676b71f2b3
fendy <fendy.fendy770@gmail.com>
2026-08-12 13:30:27 +0700

    fix: tambahkan cache-busting timestamp pada file aset JS

--- app/Views/landing.php
+++ app/Views/landing.php
@@ -274,6 +274,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="<?= base_url('assets/js/landing.js') ?>"></script>
+    <script src="<?= base_url('assets/js/landing.js?v=' . time()) ?>"></script>
 </body>
 </html>
